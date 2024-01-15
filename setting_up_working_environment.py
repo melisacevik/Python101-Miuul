1a62e7232edc2dd7efa28fd891d3f676d4b856f5
Melisa Çevik <celissamevik@gmail.com>
2024-01-15 11:13:20 +0300

    Initial commit

---
+++ setting_up_working_environment.py
@@ -0,0 +1,17 @@
+#sanal ortamların listelenmesi:
+# conda env list
+
+#sanal ortam oluşturma:
+#conda create -n myenv
+
+#sanal ortamı aktif etme:
+#conda activate myenv
+
+#Yüklü paketlerin listelenmesi:
+#conda list
+
+#Paket yükleme:
+# conda install numpy
+
+#Aynı anda birden fazla paket yükleme:
+# conda install numpy scipy pandas
