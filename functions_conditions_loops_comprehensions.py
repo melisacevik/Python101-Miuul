bbe54dda0d94deff121b5410a02034996fd42b0c
Melisa Çevik <celissamevik@gmail.com>
2024-01-17 15:05:15 +0300

    adding mülakat sorusu

--- functions_conditions_loops_comprehensions.py
+++ functions_conditions_loops_comprehensions.py
@@ -330,7 +330,28 @@ print(students)
 print(A)
 print(B)
 
+################
+# Uygulama - Mülakat Sorusu
+################
+#divide_students fonksiyonu yazınız.
+#Çift indexte yer alan öğrencileri bir listeye alınız.
+#Tek indexte yer alan öğrencileri bir listeye alınız.
+#Fakat bu iki liste tek bir liste olarak return olsun.
 
+students = ['John', 'Mark', 'Vanessa', 'Mariam']
 
+def divide_students(students):
+    groups = [[],[]]
+    for index,student in enumerate(students):
+        if index % 2 == 0:
+            groups[0].append(student)
+        else:
+            groups[1].append(student)
+    print(groups)
+    return groups
+
+st = divide_students(students)
+st[0]
+st[1]
 
 
