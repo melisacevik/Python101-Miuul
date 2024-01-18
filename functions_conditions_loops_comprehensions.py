a620609ada41ce9c9eb804823f03ba340ac647b9
Melisa Çevik <celissamevik@gmail.com>
2024-01-18 22:10:03 +0300

    adding List Comprehensions

--- functions_conditions_loops_comprehensions.py
+++ functions_conditions_loops_comprehensions.py
@@ -435,3 +435,60 @@ list(filter(lambda x: x % 2 == 0, list_store))
 from functools import reduce
 list_store = [1,2,3,4]
 reduce(lambda a,b: a+b, list_store) # peş peşe elemanları ekler
+
+
+################
+# COMPREHENSIONS
+################
+
+#
+# List Comprehension : Bu yapı ile tek satırda, hem bir döngü, hem bir if else, hem de belirli bir fonksiyonu , döngü elemanlarına uygulama işlemi sağlar.
+#
+def new_salary(x):
+    return x * 20 / 100 + x
+
+salaries = [1000, 2000, 3000, 4000]
+
+null_list = []
+
+for salary in salaries:
+    null_list.append(new_salary(salary))
+
+null_list = []
+
+for salary in salaries:
+    if salary > 3000:
+        null_list.append(new_salary(salary))
+    else:
+        null_list.append(new_salary(salary * 2))
+
+
+## bu işlemlerin yerine aşağıdaki çözümde aynı sonucu verecektir.
+
+[new_salary(salary * 2) if salary < 3000 else new_salary(salary) for salary in salaries]
+
+# list comprehensions nasıl oluşturulur ?
+salaries = [1000, 2000, 3000, 4000]
+[salary * 2 for salary in salaries]  #maaşları 2 ile çarpmak
+
+[salary * 2 for salary in salaries if salary < 3000 ]  # maaşı 3000'den az olanları 2 ile çarp
+
+## DİKKAT! if tek başına else olmadan çalışacaksa en sağa yazılır.
+## else ile kullanacaksak for bloğu sağ tarafta kalır.
+
+[salary * 2 if salary < 3000 else salary * 0 for salary in salaries]  # maaşı 3000'den büyük olanlar için else
+
+[new_salary(salary * 2) if salary < 3000 else new_salary(salary * 0.2) for salary in salaries]  #var olan bir fonksiyonumu kullanmak istersem? (en başa )
+
+## if tek başına olursa => [ işlem ; for ; if ]
+## if-else olursa => [ işlem ; if-else ; for]
+
+# ÖRNEK
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+students_no = ["John", "Vanessa"]
+
+#istemediğim öğrencilerin isimlerini küçük(student_no'dakiler istemediğim) , diğerlerini büyük yazsın.
+
+[student.lower() if student in students_no else student.upper() for student in students]
