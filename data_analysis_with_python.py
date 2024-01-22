0920917024704b30345515ee74b1e05b95831937
Melisa Çevik <celissamevik@gmail.com>
2024-01-22 14:58:02 +0300

    adding NumPy - Mathematical Operations

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -193,3 +193,40 @@ v[v == 3]
 v[v != 3]
 v[v >= 3]
 
+##################
+# Matematiksel İşlemler - (Mathematical Operations)
+##################
+
+import numpy as np
+v = np.array([1,2,3,4,5])
+
+v / 5
+v * 5 /10
+v ** 2
+v - 1
+v + 1
+
+#metodlar ile de yapabiliriz;
+
+np.subtract(v, 1)
+np.add(v, 1)
+np.mean(v) #ortalama
+np.sum(v)
+np.min(v)
+np.max(v)
+np.var(v) #varyans
+
+# atamadık consoleda gözlemliyoruz. kalıcı olmasını istersen v 'ye tekrar atarsın.
+
+# NumPy ile İki Bilinmeyenli Denklem Çözümü
+
+# 5*x0 + x1 = 12
+# x0 + 3*x1 = 10
+
+
+             # x0'ın katsayıları , x1 'in katsayıları
+a = np.array([[5,1] , [1,3]])
+            # sonuc ayrı bi array'de
+b = np.array([12,10])
+
+np.linalg.solve(a,b)
