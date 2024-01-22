36f1e901d851fdb0966a5aecebe554202974f626
Melisa Çevik <celissamevik@gmail.com>
2024-01-22 12:15:50 +0300

    adding NumPy - creating numpy arrays

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -43,5 +43,22 @@ a = np.array([1, 2, 3, 4])
 b = np.array([2, 3, 4, 5])
 a * b
 
+##################
+# NumPy Array'i oluşturma - creating numpy arrays
+##################
+
+import numpy as np
+
+np.array([1, 2, 3, 4, 5])
+type(np.array([1, 2, 3, 4, 5])) # class 'numpy.ndarray'
+
+
+np.zeros(10, dtype=int) #(adet,veri yapısı) | # 0 'dan oluşan ndarray oluşturmak istersem;
+np.random.randint(0, 10, size=10) #(başlangıç aralık,bitiş aralık, adet) integer => randint() | # belirli bir aralık olacak şekilde;
+# belirli bir istatiksel dağılıma göre sayı üretmek istersek normal() kullanırız
+np.random.normal(10, 4,(3,4)) #(normaldağılımlıkitleortalaması, argüman=> ör stand sapm = 4 , boyut)
+# 3 satır 4 sütundan oluşan ortalaması 10 standart sapması 4 olacak şekilde normal dağılımlı sayılar oluşturuldu.
+
+
 
 
