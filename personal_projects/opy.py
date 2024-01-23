fd8a2b26c580f29e96b4624634a2f50b969f813b
Melisa Çevik <celissamevik@gmail.com>
2024-01-23 21:44:01 +0300

    adding README.md

--- personal_projects/opy.py
+++
@@ -1,9 +0,0 @@
-## Şehirlere göre hava durumu tahmini
-## terminale pip3 install requests yazdık
-## bu kütüphane sayesinde API'lere HTTP istekleri atabiliriz.
-
-import requests
-import json
-
-location = input("şehir arayınız.")
-
