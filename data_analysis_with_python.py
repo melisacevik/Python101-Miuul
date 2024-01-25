5cf03deb07f52c43fd1139cd997f646fa9935c83
Melisa Çevik <celissamevik@gmail.com>
2024-01-25 22:54:08 +0300

    adding Veri Görselleştirme - Sayısal Degisken

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -733,3 +733,22 @@ df['sex'].value_counts().plot(kind='bar')
 # kind = bar ( sütun ) bilgisi
 # grafiği plt.show() ile ekrana basarız.
 plt.show()
+
+##########################
+# Sayısal Degisken Görsellestirme
+##########################
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+plt.hist(df["age"])
+plt.show()
+
+plt.boxplot(df["fare"])
+plt.show()
