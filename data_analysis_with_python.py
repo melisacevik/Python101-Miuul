a8b0b764c1758160f2f7893597c5a1720a988517
Melisa Çevik <celissamevik@gmail.com>
2024-01-27 02:14:34 +0300

    adding eda | Değişkenlerin yakalanması

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -1106,3 +1106,127 @@ num_summary(df, "age", plot=True)
 
 for col in num_cols:
     num_summary(df, col, plot=True)
+
+######################
+# Değişkenlerin Yakalanması ve İşlemlerin Genelleştirilmesi
+######################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+df.info()
+
+
+# cat_th => numerik ve kategorik olanlar
+# 10 dan küçükse kategorik
+# eşsiz sayısı 20 den büyükse kardinal
+
+# docstring => help(grab_col_names) dediğimizde bu docstring gelir
+def grab_col_names(dataframe, cat_th=10, car_th=30):
+    """
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Parameters
+    ----------
+    dataframe : dataframe
+        değişken isimleri alınmak istenen dataframedir.
+    cat_th: int,float
+        numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+    car_th: int,float
+        kategorik fakat kardinal değişkenler için sınıf eşik değeri
+    Returns
+    -------
+    cat_cols: list
+        Kategorik değişken listesi
+    num_cols: list
+        Numerik değişken listesi
+    car_but_car : list
+        Kategorik görünümlü kardinal değişken listesi
+
+    Notes
+    --------
+    cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+    num_but_cat cat_cols'un içinde
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+    num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int", "float"]]
+    cat_but_car = [col for col in df.columns if
+                   df[col].nunique() > 20 and str(df[col].dtypes) in ["category", "object"]]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in df.columns if df[col].dtype in ["int", "float"]]
+    num_cols = [col for col in num_cols if col not in cat_cols]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_cols: {len(cat_but_car)}")
+    print(f"num_but_cols: {len(num_but_cat)}")
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+def cat_summary(dataframe, col_name):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+
+    print("###################")
+
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist()
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+# bonus
+df = sns.load_dataset("titanic")
+df.info()
+for col in df.columns:
+    if df[col].dtype == 'bool':
+        df[col] = df[col].astype(int)  # bool'u int yaptık cünkü grafik okutamıyor derste
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+def cat_summary(dataframe, col_name, plot=False):
+    # çıktının düzgün bir formda olmasını istediğim için data frame e koyduk
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+
+    print("###################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+
+for col in cat_cols:
+    cat_summary(df, col, plot=True)
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
