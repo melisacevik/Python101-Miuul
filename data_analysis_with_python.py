b23af66a077fb62a70b3a943ded729139cf1bcbd
Melisa Çevik <celissamevik@gmail.com>
2024-01-27 01:24:17 +0300

    adding eda | Analysis of Numerical Variables

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -1046,3 +1046,63 @@ for col in cat_cols:
 #        cat_summary(df, col, plot=True)
 #   else:
 #        cat_summary(df, col, plot=True)
+
+######################
+# Sayısal Değişken Analizi | Analysis of Numerical Variables
+######################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int", "float"]]
+cat_but_car = [col for col in df.columns if df[col].nunique() > 20 and str(df[col].dtypes) in ["category", "object"]]
+cat_cols = cat_cols + num_but_cat
+cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+df[["age", "fare"]].describe().T
+
+# veri setinin içinden numeric değişkenleri nasıl seçerim?
+
+num_cols = [col for col in df.columns if df[col].dtype in ["int", "float"]]
+# bazıları kateg. değişken ama.
+
+# num_cols'un içinde olup, cat_cols'da olmayan ( cat_cols => kategorik )
+num_cols = [col for col in num_cols if col not in cat_cols]
+
+
+def num_summary(dataframe, numerical_col):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+
+num_summary(df, "age")
+
+for col in num_cols:
+    num_summary(df, col)
+
+
+# görselleştirme ekle
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist()
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+num_summary(df, "age", plot=True)
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
