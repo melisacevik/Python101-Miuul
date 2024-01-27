4b0f0e19d1401ace28dcc518e3d2be217facfc2c
Melisa Çevik <celissamevik@gmail.com>
2024-01-27 11:03:08 +0300

    adding eda | Analysis of Target Variable | cat & num

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -1128,7 +1128,7 @@ df.info()
 # eşsiz sayısı 20 den büyükse kardinal
 
 # docstring => help(grab_col_names) dediğimizde bu docstring gelir
-def grab_col_names(dataframe, cat_th=10, car_th=30):
+def grab_col_names(dataframe, cat_th=10, car_th=20):
     """
     Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
     Parameters
@@ -1230,3 +1230,129 @@ for col in cat_cols:
 
 for col in num_cols:
     num_summary(df, col, plot=True)
+
+######################
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+######################
+# amaç; target'ı kategorik ve sayısal değişkenler açısından analiz etmek.
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+
+
+# bool'u int yaptığı fonksiyonu atla!
+
+def cat_summary(dataframe, col_name, plot=False):
+    # çıktının düzgün bir formda olmasını istediğim için data frame e koyduk
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+
+    print("###################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Parameters
+    ----------
+    dataframe : dataframe
+        değişken isimleri alınmak istenen dataframedir.
+    cat_th: int,float
+        numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+    car_th: int,float
+        kategorik fakat kardinal değişkenler için sınıf eşik değeri
+    Returns
+    -------
+    cat_cols: list
+        Kategorik değişken listesi
+    num_cols: list
+        Numerik değişken listesi
+    car_but_car : list
+        Kategorik görünümlü kardinal değişken listesi
+
+    Notes
+    --------
+    cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+    num_but_cat cat_cols'un içinde
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+    num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int", "float"]]
+    cat_but_car = [col for col in df.columns if
+                   df[col].nunique() > 20 and str(df[col].dtypes) in ["category", "object"]]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in df.columns if df[col].dtype in ["int", "float"]]
+    num_cols = [col for col in num_cols if col not in cat_cols]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_cols: {len(cat_but_car)}")
+    print(f"num_but_cols: {len(num_but_cat)}")
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+##--
+df.head()
+df["survived"].value_counts()
+cat_summary(df, "survived")
+
+# insanların hayatta kalma durumunu etkileyen şey nedir?
+
+
+######################
+# Hedef Değişkenin Kategorik Değişkenler ile Analizi
+######################
+
+# cinsiyete göre hayatta kalma oranları inceleyelim.
+
+df.groupby("sex")["survived"].mean()
+
+
+# target'ı bir kategorik değişkeni otomatik olarak nasıl inceleriz?
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}))
+
+
+target_summary_with_cat(df, "survived", "sex")  # cinsiyetin targeta etkisi
+target_summary_with_cat(df, "survived", "pclass")  # sınıfların targeta etkisi
+
+# cat_cols ile kategorik değişkenleri belirlemiştik,
+
+for col in cat_cols:
+    target_summary_with_cat(df, "survived", col)
+
+######################
+# Hedef Değişkenin Sayısal Değişkenler ile Analizi
+######################
+
+# kategorik işlemin tam tersi
+# burada hayatta kalma durumuna göre yaş ortalamaları inceleyeceğiz. o yüzden groupby(target)
+
+df.groupby("survived")["age"].mean()
+
+df.groupby("survived").agg({"age": "mean"})  # ["age"].mean() ile aynı ama bunun çıktısı daha düzgün
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+
+target_summary_with_num(df, "survived", "age")
